9ff1c7c05e5babbd8859901bc661e1915ffc4047
yuxin-cmd <1994334223@qq.com>
2026-05-20 02:22:41 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: import.meta.dirname,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
