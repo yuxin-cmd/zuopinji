b3a205a5d14c087ae71581ed74c2a7b948234a17
yuxin-cmd <1994334223@qq.com>
2026-05-21 11:58:03 +0800

    修复 next.config.ts 配置

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  turbopack: {
+    root: import.meta.dirname,
+  },
+};
 
 export default nextConfig;
